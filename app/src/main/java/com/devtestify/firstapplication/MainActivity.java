82d4de89faf2b7236202d83ca01b420887b5c88b
Mr.AJ <jenilambaliya999@gmail.com>
2026-05-21 14:02:51 +0530

    Implement counter functionality and update main layout with increment and decrement buttons

--- app/src/main/java/com/devtestify/firstapplication/MainActivity.java
+++ app/src/main/java/com/devtestify/firstapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.devtestify.firstapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView counterText;
+    Button addButton, subButton;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        counterText = findViewById(R.id.counterText);
+        addButton = findViewById(R.id.addButton);
+        subButton = findViewById(R.id.subtractButton);
+        counter = 0;
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                counterText.setText("Total counter is : " + counter);
+            }
+        });
+
+        subButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                counterText.setText("Total counter is : " + counter);
+            }
         });
     }
 }

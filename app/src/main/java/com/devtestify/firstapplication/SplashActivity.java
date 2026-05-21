05febd8f0cfc1f32c0872e6448a5dd8be3f1d387
Mr.AJ <jenilambaliya999@gmail.com>
2026-05-21 18:54:27 +0530

    Implement navigation from SplashActivity to MainActivity after delay and finish activity on pause

--- app/src/main/java/com/devtestify/firstapplication/SplashActivity.java
+++ app/src/main/java/com/devtestify/firstapplication/SplashActivity.java
@@ -1,11 +1,13 @@
 package com.devtestify.firstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +17,12 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(5000);
+
                 } catch (Exception e) {
 
                 } finally {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
 
                 }
                 super.run();
@@ -27,4 +32,9 @@ public class SplashActivity extends AppCompatActivity {
         thread.start();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 }
